670331001763ff162d50dbc0173a0610149a4ec2
Anay Infowind <anay.infowind@gmail.com>
2022-04-11 13:25:38 +0530

    fixed lang file

--- lang/en/local_leeloolxp_web_login_tracking.php
+++ lang/en/local_leeloolxp_web_login_tracking.php
@@ -47,7 +47,7 @@ $string['no'] = "No";
 $string['ok'] = "Ok";
 $string['still_learning'] = 'Are you still learning?';
 $string['still_learning_yes'] = 'Yes!';
-$string['still_learning_no'] = "No, I\'m not";
+$string['still_learning_no'] = "No, I'm not";
 
 $string['privacy:metadata'] = 'In order to integrate with Leeloo LXP, some user
 data need to be sent to the Leeloo LXP client application (remote system).';
